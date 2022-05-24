15a16546b47daf76cc49c7f8500cac2bd1b16cc2
Zhekov Georgi (HAU-ITDW) <Georgi.Zhekov2@liebherr.com>
2022-05-24 20:02:19 +0300

    added db an data access projects

---
+++ TradingApp/SupportLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace SupportLibrary.DataAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        //Used when data should be loaded
+        //If sp. makes some changes but also returns data LoadData should be used
+        public async Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<T>(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure);
+                return rows.ToList();
+            }
+        }
+        //Used when data should be saved, changed, added, deleted to the db
+        public async Task SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public async Task<List<T>> LoadDataTest<T, U>(string query, string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+               var rows = await connection.QueryAsync<T>(query, commandType: CommandType.Text);
+
+               return rows.ToList();
+            }
+        }
+    }
+}
