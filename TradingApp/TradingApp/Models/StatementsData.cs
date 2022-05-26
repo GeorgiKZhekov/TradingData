00c53a265cbbe2caf3ff94b86d329c839363d6c1
Zhekov Georgi (HAU-ITDW) <Georgi.Zhekov2@liebherr.com>
2022-05-26 14:12:50 +0300

    take data from tables

---
+++ TradingApp/TradingApp/Models/StatementsData.cs
@@ -0,0 +1,10 @@
+﻿namespace TradingApp.Models
+{
+    public class StatementsData
+    {
+        public string Headers { get; set; }
+        public string Sections { get; set; }
+        public string Data { get; set; }
+
+    }
+}
